121053c53f6b8e3424a5aa19b6d14b1b96a3e70b
Ryan Catlin <ryan.catlin@gmail.com>
2021-02-16 01:08:49 -0500

    Do not commit templated asset

--- assets/custom_start_valheim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export templdpath=$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
-export SteamAppId=892970
-
-# Tip: Make a local copy of this script to avoid it being overwritten by steam.
-# NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-./valheim_server.x86_64 -name "GoVikingGo" -port 2456 -nographics -batchmode -world "GoVikingGo" -password "iceiceviking"
-export LD_LIBRARY_PATH=$templdpath
