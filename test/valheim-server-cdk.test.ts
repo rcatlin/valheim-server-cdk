a29d80d9ff16eb4151411a04c020da05f61d39c3
Ryan Catlin <ryan.catlin@gmail.com>
2021-02-22 17:48:33 -0500

    Update test to pass and check for EC2 Instance

--- test/valheim-server-cdk.test.ts
+++ test/valheim-server-cdk.test.ts
@@ -1,21 +1,17 @@
 import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import { InstanceClass, InstanceSize } from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
 import * as ValheimServerCdk from '../lib/valheim-server-cdk-stack';
 
-test('SQS Queue Created', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new ValheimServerCdk.ValheimServerCdkStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
-    }));
-});
-
-test('SNS Topic Created', () => {
+test('EC2 Instance Created', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new ValheimServerCdk.ValheimServerCdkStack(app, 'MyTestStack');
+  const stack = new ValheimServerCdk.ValheimServerCdkStack(app, 'MyTestStack', {
+    keyPairName: 'foo-key-pair-name',
+    instanceSize: InstanceSize.LARGE,
+    instanceClass: InstanceClass.T2,
+    backupS3BucketName: 'foo-backup-bucket-name'
+  });
   // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+  expectCDK(stack).to(haveResource("AWS::EC2::Instance"));
 });
